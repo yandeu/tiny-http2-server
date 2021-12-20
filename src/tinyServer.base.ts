8734a812e942e7906c3bd63b756175f068c8a795
yandeu <20306025+yandeu@users.noreply.github.com>
2021-12-20 10:07:11 +0100

    Force close connections

--- src/tinyServer.base.ts
+++ src/tinyServer.base.ts
@@ -19,6 +19,7 @@ export abstract class TinyServerBase<
 > {
   public router = new Router()
   private server!: Srv
+  private sockets: Map<string, Socket> = new Map()
   /** alias for this.router.route */
   public r = this.router.route
   /** alias for this.router.route */
@@ -43,6 +44,15 @@ export abstract class TinyServerBase<
   public listen(port: Port = this.randomPort()): Promise<Port> {
     return new Promise(resolve => {
       this.server = this.createServer() as Srv
+
+      this.server.on('connection', socket => {
+        const randomId = Math.random().toString(32).substring(2) + '-' + Math.random().toString(32).substring(2)
+        this.sockets.set(randomId, socket)
+        socket.on('close', () => {
+          this.sockets.delete(randomId)
+        })
+      })
+
       this.server.listen(port, () => {
         // graceful shutdown
         process.on('SIGTERM', async () => {
@@ -55,10 +65,20 @@ export abstract class TinyServerBase<
     })
   }
 
-  public close(): Promise<void> {
+  public close(force = true, timeout = 2000): Promise<void> {
     return new Promise(resolve => {
+      const t = setTimeout(() => {
+        if (force) {
+          console.log('Force destroy sockets.')
+          for (const [key, socket] of this.sockets) {
+            socket.destroy()
+          }
+        }
+      }, timeout)
+
       this.server.close(() => {
-        resolve()
+        clearTimeout(t)
+        return resolve()
       })
     })
   }

761cf96f2af91b2bd022ec10ae39e51711591986
yandeu <20306025+yandeu@users.noreply.github.com>
2021-12-20 21:37:23 +0100

    Parse body from buffer and string

--- src/middleware/bodyParser.ts
+++ src/middleware/bodyParser.ts
@@ -4,16 +4,19 @@ import { ExpressHandler, NextFunction, Response } from '../types'
 /** Parses body and adds content to req.body */
 const parseBody = async (req: Request | RequestHttp2): Promise<void> =>
   new Promise((resolve, reject) => {
-    let data = ''
+    let buffers: Buffer[] = []
+    let string: string = ''
 
     // check if is http2 stream
     const _req = (req as RequestHttp2).stream ? (req as RequestHttp2).stream : req
 
     _req
       .on('data', chunk => {
-        data += chunk
+        if (Buffer.isBuffer(chunk)) buffers.push(chunk)
+        else if (typeof chunk === 'string') string += chunk
       })
       .on('end', () => {
+        const data = buffers.length > 0 ? Buffer.concat(buffers) : string
         req.body = data
         return resolve()
       })
@@ -23,17 +26,26 @@ const parseBody = async (req: Request | RequestHttp2): Promise<void> =>
   })
 
 export const bodyParser = {
+  /** Parse json and text. */
   json: () =>
     (async (req: Request, res: Response, next: NextFunction) => {
       await parseBody(req)
 
       // isJSON
-      if (req.headers['content-type'] !== 'application/json') return next()
+      const isJSON = req.headers['content-type'] === 'application/json'
+      const isText = req.headers['content-type']?.startsWith('text/')
+      if (!isJSON && !isText) return next()
 
       // replace body with json
       try {
-        const json = JSON.parse(req.body)
-        req.body = json
+        // to string
+        const string = (req.body as Buffer | string).toString()
+        req.body = string
+        // to json
+        if (isJSON) {
+          const json = JSON.parse(string)
+          req.body = json
+        }
       } catch (error) {}
 
       return next()
